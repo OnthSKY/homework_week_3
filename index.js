40225e85a9b19481ce2cddf522cabff5e3aa0b9a
SKY <onthsky@gmail.com>
2022-10-14 14:39:28 +0300

    middleware has added for POST operation by SKY

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
-
+app.use(function(req,res,next){
+    if (req.method === 'POST'){
+        console.log("Post isteği için istek gönderildi");
+    }
+    next();
+});
 app.get('/hello', function(req, res){
     res.send("Merhaba, GET isteği attınız");
 });
 app.post('/hello', function(req, res){
-    console.log("Post isteği için istek gönderildi");
+   
     res.send("Merhaba, POST isteği attınız");
 });
 
