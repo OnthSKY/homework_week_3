f63d31242b401b67181c4393bc350b94332c3c77
SKY <onthsky@gmail.com>
2022-10-14 14:28:54 +0300

    CRUD operation codes has added

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.get('/hello', function(req, res){
+    res.send("Merhaba, GET isteği attınız");
+});
+app.post('/hello', function(req, res){
+    res.send("Merhaba, POST isteği attınız");
+    console.log("Post isteği için istek gönderildi");
+});
+
+app.put('/hello', function(req, res){
+    res.send("Merhaba, PUT isteği attınız");
+});
+
+app.delete('/hello', function(req, res){
+    res.send("Merhaba, DELETE isteği attınız")
+});
+
+const server = app.listen(8081, function(){
+    var host = server.address().address;
+    var port = server.address.port;
+    console.log("App listening at http://%s:%s", host,port);
+});
